116bf4131812eafd13f1bf5f7666c5820f509f0c
leaf-lxh <meleaf@qq.com>
2020-08-11 00:31:15 +0800

    修正chmod路径错误

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "【正在创建配置目录/etc/NekoRPC】"
+echo "[正在创建配置目录/etc/NekoRPC]"
 mkdir /etc/NekoRPC/
 
-echo "【正在复制文件】"
+echo "[正在复制文件]"
 cp files/nekorpc.conf /etc/NekoRPC/
-chmod 644 /etc/NekoRPC/files/nekorpc.conf
+chmod 644 /etc/NekoRPC/nekorpc.conf
 
-echo "【完成】"
+echo "[完成]"
